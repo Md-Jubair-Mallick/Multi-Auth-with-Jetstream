a70106069812c82236a2b040b38c5ec61c2ee369
MD Jubair <mallickblogger@gmail.com>
2024-12-11 20:53:33 +0600

    Add Role Validation & Password Validation During Registration

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -24,12 +24,14 @@ class CreateNewUser implements CreatesNewUsers
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => $this->passwordRules(),
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
+            'role'=> ['required', 'in:admin,editor,viewer']
         ])->validate();
 
         return User::create([
             'name' => $input['name'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
+            'role' => $input['role']
         ]);
     }
 }
